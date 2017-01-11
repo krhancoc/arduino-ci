6c41a0763afc3cb72b3837940e3321f106237657
Kenneth Hancock <kenneth.hancock@westjet.com>
2017-01-11 12:53:06 -0700

    Refined the bash script a bit more -- no build just yet

--- bin/integration.sh
+++ bin/integration.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+ROOT=$(pwd)
+FILE=$(pwd)/files/$1
+mkdir temp
+cd temp && ino init
+mv $FILE src/sketch.ino
+#ino build
 
-cd ..
-FILE=files/$1
 
-mkdir temp && cd temp
-ino init
-mv $FILE src/
-ino build
+#Clean up
+cd $ROOT
+rm -r -f temp/
 
