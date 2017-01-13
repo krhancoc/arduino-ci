332c3b7270394d3e73727b7a3a8fa962104ce20f
Kenneth Hancock <kenneth.hancock@westjet.com>
2017-01-13 11:33:23 -0700

    Lots of documentation added - also pulled the tool out to a more Object oriented approach

--- server.py
+++ server.py
@@ -1,3 +1,20 @@
+""" Arduino Continuous Integration Tool Server for Mac-OS/Linux
+
+This server allows for multiple developers to work on one Arduino Chip that could be attached
+to a robot that you may not necessarily have two of!
+
+Please follow the README.md for start up instruction and problems you may run into.
+
+Attributes:
+    UPLOAD_FOLDER (str): Folder in which uploads will temporarily be stored
+    ALLOWED_EXTENSIONS (set): List of allowed extensions the server will take
+    app (object): The flask app server
+
+TODO:
+    Add Support for libraries
+    Add UI too allow different libraries to be loaded with the build.
+"""
+
 import os
 import subprocess
 import logging
@@ -12,9 +29,51 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.secret_key = "WHATEVERYOUWANTBRO"
 
 
-def process(filename):
-    app.logger.debug("Calling integration.sh script with filename: " + filename)
-    subprocess.call("bin/integration.sh " + filename, shell=True)
+class InoTool():
+    """Tool used to build and upload files handed to it by the server
+
+    Attributes:
+        upload_lock (bool): Lock that is set when a user is uploading. Stops other users from uploading as well.
+
+    """
+
+    def __init__(self):
+        """Tool will only init the lock attribute"""
+        self.upload_lock = False
+
+    def is_locked(self):
+        """Checks if upload lock is locked"""
+        return self.upload_lock
+
+    def lock(self):
+        """Locks upload lock"""
+        self.upload_lock = False
+
+    def unlock(self):
+        """Unlocks upload lock"""
+        self.upload_lock = True
+
+    def process(self, filename):
+        """Kicks of build and upload process by calling integration.sh within bin/
+
+        Args:
+            filename (str): Filename that will be passed to the integration.sh script
+
+        Returns:
+            bool: Returns if the script errors or not any any point.  True for Success.
+
+        """
+
+        self.lock()
+        app.logger.debug("Calling integration.sh script with filename: " + filename)
+        if (subprocess.call("bin/integration.sh " + filename, shell=True) is 0):
+            app.logger.error("Problem Running Integration Script")
+            self.unlock()
+            return False
+        else:
+            self.unlock()
+            app.logger.info("Integration script run without error")
+            return True
 
 
 # http://flask.pocoo.org/docs/0.12/patterns/fileuploads/
@@ -32,7 +91,7 @@ def index():
 # http://flask.pocoo.org/docs/0.12/patterns/fileuploads/
 @app.route('/submit', methods=['POST'])
 def upload_file():
-    if request.method == 'POST':
+    if request.method == 'POST' and not app.tool.is_locked():
         if 'ino' not in request.files:
             app.logger.error("Non ino file attempted to be uploaded")
             flash('Not and INO file')
@@ -46,10 +105,11 @@ def upload_file():
             filename = secure_filename(file.filename)
             app.logger.info("File " + filename + " accepted.  Moving through to build and upload")
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            process(filename)
+            app.tool.process(filename)
             flash('Success!')
             return redirect(url_for('index'))
 
 
 if __name__ == "__main__":
+    app.tool = InoTool()
     app.run(debug=True)
