148b4eb4590730500e59113a004ff7958149e926
Kenneth Hancock <kenneth.hancock@westjet.com>
2017-01-13 10:18:13 -0700

    Gitignore template used

--- bin/integration.sh
+++ bin/integration.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 ROOT=$(pwd)
 FILE=$(pwd)/files/$1
+
 mkdir temp
 echo $FILE moved to temporary project... calling ino init...
+
 cd temp && ino init
 mv $FILE src/sketch.ino
 cp $ROOT/bin/ino.ini $ROOT/temp/ino.ini
+
 echo Copying done... calling ino build
 ino build
 ino upload
+
 echo Cleaning up...
-#Clean up
 cd $ROOT
 rm -r -f temp/
 
--- server.py
+++ server.py
@@ -5,8 +5,8 @@ import logging
 from flask import Flask, render_template, request, redirect, url_for, flash
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER='files'
-ALLOWED_EXTENSIONS = set(['ino'])
+UPLOAD_FOLDER = 'files'
+ALLOWED_EXTENSIONS = {'ino'}
 app = Flask(__name__, template_folder='templates', static_url_path='/static')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.secret_key = "WHATEVERYOUWANTBRO"
@@ -16,15 +16,18 @@ def process(filename):
     app.logger.debug("Calling integration.sh script with filename: " + filename)
     subprocess.call("bin/integration.sh " + filename, shell=True)
 
+
 # http://flask.pocoo.org/docs/0.12/patterns/fileuploads/
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
 @app.route('/')
 def index():
     logging.debug("Rendering index.html template")
     return render_template('index.html')
+
 
 # http://flask.pocoo.org/docs/0.12/patterns/fileuploads/
 @app.route('/submit', methods=['POST'])
@@ -47,8 +50,6 @@ def upload_file():
             flash('Success!')
             return redirect(url_for('index'))
 
+
 if __name__ == "__main__":
-    app.run(debug=True)    
-   
-
-
+    app.run(debug=True)
